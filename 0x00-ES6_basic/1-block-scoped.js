a63c392eb62caecb29fe8a8c2bc2e1c0c1568be2
root <Kazenzi>
2023-01-23 17:52:42 +0000

    mm

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,17 @@
+export default function taskBlock(trueOrFalse) {
+
+  const task = false;
+
+  const task2 = true;
+
+
+
+  if (trueOrFalse) {
+
+    return [task, task2];
+
+  }
+
+  return [task, task2];
+
+}
---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,17 @@
+export default function appendToEachArrayValue(array, appendString) {
+
+  const arr = [];
+
+  for (const idx of array) {
+
+    const value = idx;
+
+    arr.push(appendString + value);
+
+  }
+
+
+
+  return arr;
+
+}
